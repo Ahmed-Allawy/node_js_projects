4a57254ab51df2724abdc8e590b67b6bf844b989
Ahmed-Allawy <m_ahmed20156947@yahoo.com>
2023-09-24 18:19:32 +0300

    done!

--- Lab5_simple_server_with_Authentication&Authorization/router/todos.js
+++ Lab5_simple_server_with_Authentication&Authorization/router/todos.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Todo = require('../model/todos') 
 const authorizationMiddleware = require('../middleware/authorizationMiddleware');
-// router.get('/:userId',async(req,res,next)=>{
-//   try {
-//     const str = req.params.userId;
-//     const id = str.replace(/:/g, "");
-//     console.log(id);
-//     const todos = await Todo.find({userId:id}, {title:1,status:1,tags:1,_id:0});
-//     res.json(todos);
-//   } catch (error) {
-//     error.statusCode = 401;
-//     next(error);
-//   }
-// });
+
+router.get('/all', authorizationMiddleware,
+async(req,res,next)=>{
+  try { 
+    const todos = await Todo.find({}, {title:1,status:1,tags:1,_id:1});
+    res.json(todos);
+  } catch (error) {
+    error.statusCode = 401;
+    next(error);
+  }
+});
+
 router.post('/',
 authorizationMiddleware,
 async (req,res, next)=>{
@@ -31,7 +31,10 @@ async (req,res, next)=>{
     next(error);
   }
 });
-router.delete('/:id',async (req,res, next)=>{
+
+router.delete('/:id',
+authorizationMiddleware,
+async (req,res, next)=>{
   try {
     const str = req.params.id;
     const id = str.replace(/:/g, "");
@@ -39,16 +42,19 @@ router.delete('/:id',async (req,res, next)=>{
     const todo = await Todo.findByIdAndDelete(id);
 
     if (!todo) {
-      return res.status(404).json({ error: 'Todo not found' });
+      throw new Error('todo not found')
     }
 
     res.json({ message: 'todo was deleted successfully' });
   } catch (error) {
-    error.statusCode = 422;
+    error.statusCode = 401;
     next(error);
   }
 });
-router.patch('/:id',async (req,res, next)=>{
+
+router.patch('/:id',
+authorizationMiddleware,
+async (req,res, next)=>{
   try {
     const str = req.params.id;
     const id = str.replace(/:/g, "");
@@ -62,28 +68,25 @@ router.patch('/:id',async (req,res, next)=>{
     );
 
     if (!todo) {
-      return res.status(404).json({ error: 'todo not found' });
+      throw new Error('todo not found')
     }
 
     res.json({ message: 'todo was edited successfully', todo });
   } catch (error) {
-    error.statusCode = 422;
+    error.statusCode = 401;
     next(error);
   }
 });
 
-
 router.get('/',
 authorizationMiddleware,
 async(req,res,next)=>{
-  try {
-   
+  try { 
     const id = req.id;
-
     const { limit = 10, skip = 0 } = req.query;
     const limitNum = parseInt(limit, 10);
     const skipNum = parseInt(skip, 10);
-    const todos = await Todo.find({userId:id}, {title:1,status:1,tags:1,_id:0})
+    const todos = await Todo.find({userId:id}, {title:1,status:1,tags:1,_id:1})
     .limit(limitNum)
     .skip(skipNum);;
     res.json(todos);
@@ -92,4 +95,5 @@ async(req,res,next)=>{
     next(error);
   }
 });
+
 module.exports = router;
