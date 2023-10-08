f1fa6dbda1c9be28bbcbff564645a3f035ddc6ba
Ahmed-Allawy <m_ahmed20156947@yahoo.com>
2023-10-08 04:22:27 +0300

    rename

---
+++ Book_api/controller/store.controller.js
@@ -0,0 +1,37 @@
+var { QueryList } = require('../db/queries');
+var { dbQuery } = require('../db/connection');
+var {generateStoreCode} = require('../util/stringGenerator');
+
+
+exports.getStoreList = async (req, res) => {
+  try {
+    var storeListQuery = QueryList.GET_STORE_LIST;
+    var result = await dbQuery(storeListQuery);
+    return res.status(200).send(JSON.stringify(result.rows));
+  } catch (err) {
+    console.log("Error : " + err);
+    return res.status(500).send({ error: 'Failed to list store' });
+  }
+
+}
+
+exports.saveNewStore = async (req, res) => {
+  try {
+    var createdBy = "admin";
+    var createdOn = new Date();
+    const { storeName, address } = req.body;
+    if (!storeName || !address) {
+      return res.status(500).send({ error: 'store name and address are required , can not be empty' })
+    }
+    let storeCode = generateStoreCode();
+
+    values = [storeName, storeCode, address, createdBy, createdOn];
+    var saveStoreQuery = QueryList.SAVE_NEW_STORE;
+    await dbQuery(saveStoreQuery, values);
+    return res.status(201).send("Successfully store created ");
+  } catch (error) {
+    console.log("Error : " + error);
+    return res.status(500).send({ error: 'Failed to list store' });
+  }
+}
+
---
+++ Book_api/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const {port} = require('./config');
+const storeRouter = require('./route/store.route');
+const bookRouter = require('./route/book.route');
+const app = express();
+
+// Custom middleware
+const requestLogger = (req, res, next) => {
+  const { method, url } = req;
+  const currentTime = new Date().toLocaleString();
+  console.log(`[${currentTime}] ${method} ${url}`);
+  next();
+};
+// Applying the middleware globally
+app.use(requestLogger);
+app.use(express.json());
+app.get('/',(req,res)=>{
+ res.send('hello allawy');
+});
+
+app.use(['/books','/book'],bookRouter);
+app.use('/store',storeRouter);
+/// error handler
+app.use((err,req,res,next)=>{
+  err.statusCode = err.statusCode || 500;
+  const handledError = err.statusCode < 500;
+  res.status(err.statusCode).send({
+    message: handledError ? err.message : "something went wronge",
+    errors: err.errors || {}
+  });
+});
+
+app.listen(port,()=>{
+  console.log('server is running');
+});
