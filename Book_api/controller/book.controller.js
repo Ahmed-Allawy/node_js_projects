2b0bb3fb8565d4e23934868140fd33b8c6ac87d1
ahmed allawy <m_ahmed20156947@yahoo.com>
2023-11-16 14:33:33 +0200

    return to project

--- Book_api/controller/book.controller.js
+++ Book_api/controller/book.controller.js
@@ -38,14 +38,12 @@ exports.getBookDetails = async (req, res) => {
   try {
     const str = req.params.bookId;
     const bookId = str.replace(/:/g, "");
-    console.log(bookId);
     if(!bookId){
       return res.status(500).send({ error: 'bookId required , can not be empty' })
   }
     values = [bookId];
     var bookDetailsQuery = QueryList.GET_BOOK_DETAILS;
     var result = await dbQuery(bookDetailsQuery, values);
-    console.log(result);
     return res.status(200).send(JSON.stringify(result.rows));
   } catch (error) {
     console.log("Error : " + error);
@@ -53,3 +51,40 @@ exports.getBookDetails = async (req, res) => {
   }
 }
 
+exports.updateBook = async (req, res) => {
+  try {
+    var createdBy = "admin";
+    var createdOn = new Date();
+    const str = req.params.bookId;
+    const bookId = str.replace(/:/g, "");
+    const { title, description, author, publisher, pages, storeCode } = req.body;
+    if(!bookId || !title || !author || !publisher || !storeCode){
+      return res.status(500).send({ error: ' bookId title , author , publisher , storeCode are required , can not be empty' })
+  }
+    values = [title, description, author, publisher, pages, storeCode, createdOn, createdBy, bookId];
+    var updateBookQuery = QueryList.UPDATE_BOOK;
+    await dbQuery(updateBookQuery, values);
+    return res.status(201).send("book updated Successfully");
+  } catch (error) {
+    console.log("Error : " + error);
+    return res.status(500).send({ error: 'Failed to update book' });
+  }
+}
+
+
+exports.deleteBook = async (req, res) => {
+  try {
+    const str = req.params.bookId;
+    const bookId = str.replace(/:/g, "");
+    if(!bookId){
+      return res.status(500).send({ error: 'bookId required , can not be empty' })
+  }
+    values = [bookId];
+    var deleteBookQuery = QueryList.DELETE_BOOK;
+    await dbQuery(deleteBookQuery, values);
+    return res.status(201).send("book deleted Successfully");
+  } catch (error) {
+    console.log("Error : " + error);
+    return res.status(500).send({ error: 'Failed to delete book' });
+  }
+}
--- Book_api/route/book.route.js
+++ Book_api/route/book.route.js
@@ -5,5 +5,6 @@ const Book = require('../controller/book.controller');
 router.get('/',Book.getBookList);
 router.get('/:bookId',Book.getBookDetails);
 router.post('/',Book.saveNewBook);
-
+router.put('/:bookId',Book.updateBook);
+router.delete('/:bookId',Book.deleteBook);
 module.exports = router;
